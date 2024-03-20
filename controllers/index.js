82fa3bf16c9db1836ac7223339eca5560b3b4c12
Randy Rebucas <rebucasrandy1986@gmail.com>
2024-03-20 11:39:15 +0800

    update

--- controllers/index.js
+++ controllers/index.js
@@ -22,8 +22,9 @@ exports.index = async (req, res, next) => {
     res.redirect("/dashboard");
   } else {
     // connection = req.mysql;
-    let r = await getStats();
-    console.log(r);
+    // let r = await checkLastLog();
+    // let r = await createLog(1234);
+    // console.log(r);
     res.render("index", {
       title: "Infusionsoft Exporter",
       infusionsoft: req.infusionsoft,
@@ -45,4 +46,34 @@ const getStats = () => {
       resolve(results)
     });
   })
+}
+
+const checkLastLog = async () => {
+  return new Promise((resolve, reject) => {
+    return connection.query(
+      `SELECT EXISTS(SELECT * from logs WHERE 'offset'=? AND type='contacts') AS isExistOffset`,
+      [(88 - 1) * 1000],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(Object.assign({}, results.shift()).isExistOffset)
+      });
+  });
+}
+
+const createLog = (lastId) => {
+  return new Promise((resolve, reject) => {
+    var log = { offset: 1000, last_id: lastId, type: 'contacts' };
+    return connection.query(
+      `INSERT INTO logs SET ?`,
+      log,
+      (error, rows) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(rows)
+      }
+    );
+  });
 }
